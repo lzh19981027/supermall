26965240055c5c7e0c459ed279ec774e75c16330
lzh19981027 <969718626@qq.com>
2020-12-25 22:28:15 +0800

    '项目的基本框架'

---
+++ src/components/content/mainTabbar/MainTabBar.vue
@@ -0,0 +1,44 @@
+<template>
+  <tab-bar>
+    <tab-bar-item path="/home">
+      <!--在src中使用别名需要在前面加'~'-->
+      <img slot="item-icon" src="~assets/img/tabbar/black/home_light.svg" alt="">
+      <img slot="item-icon-active" src="~assets/img/tabbar/red/home_light.svg" alt="">
+      <div slot="item-text">首页</div>
+    </tab-bar-item>
+    <tab-bar-item path="/category">
+      <img slot="item-icon" src="~assets/img/tabbar/black/apps.svg" alt="">
+      <img slot="item-icon-active" src="~assets/img/tabbar/red/apps.svg" alt="">
+      <div slot="item-text">分类</div>
+    </tab-bar-item>
+    <tab-bar-item path="/cart">
+      <img slot="item-icon" src="~assets/img/tabbar/black/cart_light.svg" alt="">
+      <img slot="item-icon-active" src="~assets/img/tabbar/red/cart_light.svg" alt="">
+      <div slot="item-text">购物车</div>
+    </tab-bar-item>
+    <tab-bar-item path="/profile">
+      <img slot="item-icon" src="~assets/img/tabbar/black/my_light.svg" alt="">
+      <img slot="item-icon-active" src="~assets/img/tabbar/red/my_light.svg" alt="">
+      <div slot="item-text">我的</div>
+    </tab-bar-item>
+  </tab-bar>
+</template>
+
+<script>
+
+import TabBar from "components/common/tabbar/TabBar";
+import TabBarItem from "components/common/tabbar/TabBarItem";
+
+export default {
+  name: "MainTabBar",
+  components: {
+    TabBar,
+    TabBarItem
+  }
+}
+</script>
+
+<style scoped>
+
+
+</style>
---
+++ vue.config.js
@@ -0,0 +1,17 @@
+// 导出模块
+module.exports = {
+  configureWebpack: {
+    // resolve：解决路径相关的
+    resolve: {
+      extensions: [],
+      // 配置别名，默认：'@': 'src'
+      alias: {
+        'assets': '@/assets',
+        'common': '@/common',
+        'components': '@/components',
+        'views': '@/views',
+
+      }
+    }
+  }
+}
